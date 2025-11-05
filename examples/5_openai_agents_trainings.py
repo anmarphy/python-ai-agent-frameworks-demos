57c6db3fad309de29206c5919ef3fdab958918ca
Marce HB <anmarphy@gmail.com>
2025-11-05 13:10:13 +0000

    Adding the aerialist tool

--- examples/5_openai_agents_trainings.py
+++ examples/5_openai_agents_trainings.py
@@ -10,27 +10,35 @@ set_tracing_disabled(disabled=True)
 
 # Setup the OpenAI client to use either Azure OpenAI or GitHub Models
 load_dotenv(override=True)
-API_HOST = os.getenv("API_HOST", "github")
 
 async_credential = None
-client = AsyncOpenAI(api_key=os.environ["GITHUB_TOKEN"], base_url="https://models.inference.ai.azure.com")
-MODEL_NAME = os.getenv("GITHUB_MODEL", "gpt-4o")
 
+API_HOST = os.getenv("API_HOST", "openai")
+
+if API_HOST == "github":
+    client = AsyncOpenAI(api_key=os.environ["GITHUB_TOKEN"], base_url="https://models.inference.ai.azure.com")
+    MODEL_NAME = os.getenv("GITHUB_MODEL", "gpt-4o")
+elif API_HOST == "openai":
+    client = AsyncOpenAI(api_key=os.environ["OPENAI_API_KEY"])
+    MODEL_NAME = os.environ.get("OPENAI_MODEL", "gpt-4o")
+else:
+    raise ValueError(f"Unsupported API_HOST: {API_HOST}")
 
 @function_tool
 def aerialist(element: str) -> str:
-    return {
+    if element == 'silks':
+        return {
         "element": element,
         "teacher": "Valentina",
         "level": "Beginner",
-        "place": "Liberte Aerial Arts", 
+        "place": "Liberte", 
         "duration": "8 weeks",  
         "description": f"{element} classes focus on building strength, flexibility, and foundational skills for aerial arts.",
     }
 
 agent = Agent(
     name="Aerialist agent",
-    instructions="You can only provide aerial training information using the information provided by the user.",
+    instructions="You can only provide aerial training information.",
     tools=[aerialist],
 )
 
@@ -50,14 +58,14 @@ english_agent = Agent(
 
 triage_agent = Agent(
     name="Triage agent",
-    instructions="Handoff to the appropriate agent based on the language of the request.",
+    instructions="Handoff to the appropriate agent based on the language of the request. Do not add any additional information.",
     handoffs=[spanish_agent, english_agent],
     model=OpenAIChatCompletionsModel(model=MODEL_NAME, openai_client=client),
 )
 
 
 async def main():
-    result = await Runner.run(triage_agent, input="Hola, ¿cómo estás? ¿Puedes darme informacion de las clases Telas en Liberte? La duracion y la profesora")
+    result = await Runner.run(triage_agent, input="I want to learn silks. Can you give me some information about the classes?")
     print(result.final_output)
 
     if async_credential:
